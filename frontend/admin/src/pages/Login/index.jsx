5ef8ef84bbd44ca93e12664c24c687334eb0aa71
Davi Santos <daviir17@gmail.com>
2026-04-10 09:45:40 -0300

    feat: componente para botao principal no login

--- frontend/admin/src/pages/Login/index.jsx
+++ frontend/admin/src/pages/Login/index.jsx
@@ -1,5 +1,6 @@
 import Input from '../../components/ui/input';
 import Logo from '../../components/ui/Logo';
+import PrimaryBtn from '../../components/ui/PrimaryBtn';
 import './styles.css';
 
 export default function Login() {
@@ -8,7 +9,9 @@ export default function Login() {
       <div className="login-content">
         <div className="welcome-login-section">
           <div className="welcome-login-section-content">
-            <div className='logo-login-container'><Logo/></div>
+            <div className="logo-login-container">
+              <Logo />
+            </div>
             <h1 className="welcome-login-title">
               Agenda VivaUnimed <br />
               Sistema de Fila Inteligente
@@ -16,15 +19,26 @@ export default function Login() {
           </div>
         </div>
         <div className="login-section-form">
-          <div className='title-form-container'>
+          <div className="title-form-container">
             <h1 className="form-title">Bem vindo</h1>
             <p>Acesse o Painel Administrativo para gerenciar sua agenda.</p>
           </div>
-          <div className='form-inputs-login'>
-            <Input label={'E-mail Corporativo'} styles={{minWidth: '150px'}}/>
-            <Input label={'Senha'} styles={{width: '80%', minWidth: '150px'}}/>
+          <div className="form-inputs-login">
+            <Input
+              label={'E-mail Corporativo'}
+              styles={{ minWidth: '150px' }}
+            />
+            <Input
+              label={'Senha'}
+              styles={{ width: '80%', minWidth: '150px' }}
+            />
           </div>
-          <a className='reset-password' href="">Esqueci a minha senha</a>
+          <a className="reset-password" href="">
+            Esqueci a minha senha
+          </a>
+          <div className="login-section-button">
+            <PrimaryBtn text={'Entrar no Painel'} styles={{width: '250px'}}/>
+          </div>
         </div>
       </div>
     </div>
