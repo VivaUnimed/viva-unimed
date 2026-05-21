b2a46f1c08de33f11bb4e6a755e38da70a34e43e
Kelli Tissot <kelli.tissot@gmail.com>
2026-05-21 17:19:40 -0300

    feat: match controller

--- backend/src/jobs/appointment_match.job.ts
+++ backend/src/jobs/appointment_match.job.ts
@@ -1,6 +1,7 @@
 import { IAppointment } from "shared";
 import service from "../service"
 import { CronJob } from 'cron';
+import { getMatchStrategy } from "./strategy_selector.job";
 
 interface AppointmentMatchJobOptions {
   cron: string;
@@ -23,32 +24,54 @@ export class AppointmentMatchJob {
 
   private async execute() {
     console.log("RUNNING APPOINTMENT JOB ⏳");
-    await service.request.setExpiredStatus();
-    const open = await service.appointment.listOpen();
-    for(const apt of open) {
-      // TODO: try / catch
-      await this.processAppointment(apt);
+
+    try {
+      //Invalida matches que o paciente demorou para responder
+      await service.request.setExpiredStatus();
+
+      //Invalida vagas abertas que já passaram do horário
+      await service.appointment.setExpiredPastAppointment();
+
+      //Busca apenas as vagas que restaram abertas (e no futuro)
+      const open = await service.appointment.listOpen();
+
+      for(const apt of open) {
+        try {
+          await this.processAppointment(apt);
+        } catch (e) {
+          // Captura erros individuais para não parar o processamento das outras vagas
+          console.error(`Erro ao processar appointment ID ${apt.id}:`, e);
+        }
+      }
+    } catch (e) {
+      console.error("Erro fatal na execução do job:", e);
     }
+
     console.log("APPOINTMENT JOB DONE ✅");
   }
 
   private async processAppointment(apt: IAppointment) {
     const pendingMatch = await service.appointment.hasPendingMatch(apt.id);
-    if(pendingMatch) return;
+    if (pendingMatch) return; // Se já tem notificação rodando pra essa vaga, ignora
 
-    const request = await service.request.getNextByAppointmentId(apt.id);
-    if(!request) return;
+    const strategy = getMatchStrategy(apt.date)
+
+    // Busca 1 ou 5 pessoas dependendo da estratégia
+    const requests = await service.request.getNextBatchByAppointmentId(apt.id, strategy.batchSize);
+    if (requests.length === 0) return;
 
     const expires = new Date();
-    expires.setMinutes(expires.getMinutes() + 1);
+    expires.setMinutes(expires.getMinutes() + strategy.expiresInMinutes);
 
-    const match = await service.appointment.addMatch({
+    // Cria um Match para cada pessoa selecionada
+    for (const req of requests) {
+      const match = await service.appointment.addMatch({
       appointmentId: apt.id,
-      requestId: request.id,
+      requestId: req.id,
       status: "queued",
       expiresAt: expires,
     });
 
-    console.log(`MATCH ADICIONADO APPOINTMENT=${apt.id} REQUEST=${request.id} MATCH=${match.id} EXPIRES=${expires.toISOString()}`);
+    console.log(`MATCH ADICIONADO APPOINTMENT=${apt.id} REQUEST=${req.id} MATCH=${match.id} EXPIRES=${expires.toISOString()}`);  }
   }
 }
