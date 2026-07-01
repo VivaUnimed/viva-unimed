576d68103c936591f7f09ec72a6c9726dd2f6a41
Kelli Tissot <kelli.tissot@gmail.com>
2026-06-30 21:43:48 -0300

    fix: cria db caso nao exista

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,15 +1,17 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Config } from '../config';
 import UserModel from './models/user.model';
-import { TransactionOptions } from 'sequelize';
+import { QueryTypes, TransactionOptions } from 'sequelize';
 
 export class Database {
   private sequelize!: Sequelize;
+  private config!: Config;
   constructor() {
 
   }
 
   async start(config: Config) {
+    this.config = config;
     this.sequelize = new Sequelize({
       database: config.DB_DATABASE,
       dialect: 'postgres',
@@ -29,9 +31,48 @@ export class Database {
         } : undefined,
       },
     });
+    await this.createDatabaseIfNotExists();
     await this.testConnection();
     await this.migrate();
     await UserModel.findAll();
+  }
+
+  private async createDatabaseIfNotExists() {
+    const sql = new Sequelize({
+      database: 'postgres',
+      dialect: 'postgres',
+      username: this.config.DB_USERNAME,
+      password: this.config.DB_PASSWORD,
+      host: this.config.DB_ADDRESS,
+      port: this.config.DB_PORT,
+      logging: false,
+      dialectOptions: {
+        ssl: this.config.DB_ENABLE_SSL ? {
+          require: this.config.DB_ENABLE_SSL,
+          rejectUnauthorized: false,
+        } : undefined,
+      },
+    });
+    try {
+      const result = await sql.query(
+        `SELECT 1 FROM pg_database WHERE datname = :dbName`,
+        {
+          replacements: { dbName: this.config.DB_DATABASE },
+          type: QueryTypes.SELECT,
+        }
+      );
+
+      if (result.length === 0) {
+        await sql.query(`CREATE DATABASE "${this.config.DB_DATABASE}"`);
+        console.log(`Database "${this.config.DB_DATABASE}" created.`);
+      } else {
+        console.log(`Database "${this.config.DB_DATABASE}" already exists.`);
+      }
+    } catch (e) {
+      // skip error
+    } finally {
+      await sql.close();
+    }
   }
 
   transaction(options?: TransactionOptions) {
