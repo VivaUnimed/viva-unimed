0b40d8a2c1204862233d1b345961bd4b57bca390
Kelli Tissot <kelli.tissot@gmail.com>
2026-07-21 17:44:02 -0300

    feat: criação de mais permissão

--- backend/src/entities/roles.ts
+++ backend/src/entities/roles.ts
@@ -3,13 +3,21 @@ import { Permission, PERMISSIONS, Role } from "shared";
 export const ROLES: Record<Role, readonly Permission[]> = {
   Admin: PERMISSIONS,
   Tecnico: [
-    "schedule.aprove",
+    "doctor.read",
+    "patient.read",
+    "appointment.read",
+    "schedule.approve",
+    "schedule.read",
     "user.read",
-    "speciality.read", "speciality.create", "speciality.edit",
+    "speciality.read",
+    "speciality.create",
+    "speciality.edit",
   ],
   Paciente: [
     "schedule.request",
     "speciality.read",
+    "speciality.read", //para escolher o médico
+    "appointment.read" //para ver suas próprias consultas
   ],
 } as const;
 
